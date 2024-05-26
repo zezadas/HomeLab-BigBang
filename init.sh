0d7c90be5924f2d1820b83cc19d99527f48090c3
root <root@pxe.sefod.eu>
2024-05-26 23:55:18 +0000

    add missing logic to dhcp and internet share

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -ex
+PXE_NETIF=eth1
+INT_NETIF=eth0
+
 docker build bootstrap/ -t bootstrap
 docker run -v ./data:/data bootstrap
 docker-compose build builder
@@ -9,5 +12,16 @@ docker-compose build netbootxyz-webapp
 docker-compose up -d netbootxyz-webapp
 docker-compose build dhcpd
 docker-compose up -d dhcpd
+
+#set interface ip for dhcpd server
+ip addr add 172.20.11.1/24 dev ${PXE_NETIF}
+
+#internet forwarding to pxe clients
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv4.conf.all.forwarding=1
+
+iptables -t nat -A POSTROUTING -o ${INT_NETIF} -j MASQUERADE
+iptables -A FORWARD -j ACCEPT
+iptables -A FORWARD -i eth1 -o ${INT_NETIF} -j ACCEPT
 
 
